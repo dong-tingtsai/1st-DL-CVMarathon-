47402252a0b81bc3ca239f7615d7913de3b5adda
dong-tingtsai <76948266+dong-tingtsai@users.noreply.github.com>
2025-03-30 21:42:41 +0800

    Update Day_026_Scrapy - xpath & 基本爬蟲_HW.py

--- Day_026_Scrapy - xpath & 基本爬蟲_HW.py
+++ Day_026_Scrapy - xpath & 基本爬蟲_HW.py
@@ -1,10 +1,12 @@
 import scrapy
 
-
 class EttodaySpider(scrapy.Spider):
-    name = "ettoday"
-    allowed_domains = ["www.ettoday.net"]
-    start_urls = ["http://www.ettoday.net/"]
+    name = 'ettoday'
+    allowed_domains = ['www.ettoday.net']
+    start_urls = ['https://www.ettoday.net/news/20201004/1824032.htm']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//title/text()').get()
+        content = response.xpath('//p/text()').getall()
+        print(title)
+        print(content)
